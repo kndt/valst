8c4ef1d13cd07bbb4b7ad48c8ca2242b275782c2
kndt <mnxx@protonmail.ch>
2022-11-26 00:07:18 +0000

    init

---
+++ client.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const readline = require("readline");
+const { io } = require("socket.io-client");
+
+const socket = io("http://localhost:3000");
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+let query = '';
+
+const triggerPrompt = () => {
+	rl.question("What character would you like to search for? ", term => {
+		query = term;
+		socket.emit('search', {query: term});
+		console.log(`Searching for ${term}...`);
+	});
+}
+
+socket.on('search', message => {
+	if (message.error) {
+		console.log(`ERR: No valid matches recieved for query ${query}`);
+		triggerPrompt();
+	} else{
+		const { page, resultCount, name, films } = message;
+		console.log(`(${page}/${resultCount}) ${name} - [${films}]`);
+
+		if (page === resultCount) {
+			triggerPrompt();
+		}
+	}
+});
+
+socket.on('connect', () => {
+	console.log('client connected');
+	triggerPrompt();
+});
+
+socket.on('disconnect', () => {
+	console.log('client connected');
+});
+
+socket.on('error', message => {
+	console.log(message);
+});
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Take-home Project
+
+## Techstack
+
+**_NodeJS_**
+
+
+### Setup
+Please run `npm install` to install dependencies
+
+To start the application run `npm start`.
